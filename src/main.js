33a0b31aa3bd5c0df0ae93444745f7cfa325a2b8
Peter Bunyan <pete@blueshed.co.uk>
2015-07-02 16:45:03 +0100

    version 0.0.2
    with a backwardly compatitble main.js Appl

--- src/main.js
+++ src/main.js
@@ -3,14 +3,46 @@ define(["knockout",
 		"./routes",
 		"./notify",
 		"./dialog",
-		"./connection",
-		"./store",
-		"../templates/main"],
+		"./templates/main",
+		"knockout-validation"],
 	function(ko, 
-			 Appl, Routes, notify, dialog,
-			 Connection, Store){
+			 Appl, 
+			 notify, 
+			 dialog){
 
+		Appl.prototype.init_routes = function(){
+			this.routes = new Routes(this);
+		};
 
+		Appl.prototype.add_page = function(title,action,route,href,icon){
+			return this.routes.add_to_service_menu({
+				route:route,  // the name to pass to hasher
+				title:title,  // the menu title
+				href:href,    // the href for the menu item
+				icon:icon,    // the fa icon to set in menu item
+				action:action // the function to perform
+			});
+		};
+
+		Appl.prototype.set_default_page = function(page){
+			this.routes.set_default(page);
+		};
+
+		Appl.prototype.open_dialog = function(component,params){
+			return dialog.open_dialog(component,params);
+		};
+
+		Appl.prototype.close_dialog = function(){
+			return dialog.close_dialog();
+		};
+
+		Appl.prototype.confirm = function(title,message,action,action_label,css) {
+			return dialog.confirm(title,message,action,action_label,css);
+		};
+
+		Appl.prototype.notify = function(message, type, location, fade, duration){
+			return notify(message, type, location, fade, duration);
+		};
 
 	return Appl;
 });
