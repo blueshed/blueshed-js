0f41bfa5f75273a595559521bdd335e2664c570d
Peter Bunyan <pete@blueshed.co.uk>
2015-07-06 08:53:53 +0100

    build 11
    bug in datetimepicker binding where selected date is null

--- src/bindings/ko-datetimepicker.js
+++ src/bindings/ko-datetimepicker.js
@@ -5,27 +5,32 @@ define(
 			'use strict';
 
 			ko.bindingHandlers.datetimepicker = {
-					init: function (element, valueAccessor, allBindings) {
-						var options = {
-								format: 'MM/DD/YYYY hh:mm A',
-								defaultDate: ko.unwrap(valueAccessor())
-						};
+				init: function (element, valueAccessor, allBindings) {
+					var options = {
+							format: 'MM/DD/YYYY hh:mm A',
+							defaultDate: ko.unwrap(valueAccessor())
+					};
 
-						ko.utils.extend(options, allBindings().dateTimePickerOptions);
+					ko.utils.extend(options, allBindings().dateTimePickerOptions);
 
-						$(element).datetimepicker(options).on("dp.change", function (evntObj) {
-							var observable = valueAccessor();
-							if (evntObj.timeStamp !== undefined) {
-								var picker = $(this).data("DateTimePicker");
-								var d = picker.date();
+					$(element).datetimepicker(options).on("dp.change", function (evntObj) {
+						var observable = valueAccessor();
+						if (evntObj.timeStamp !== undefined) {
+							var picker = $(this).data("DateTimePicker");
+							var d = picker.date();
+							if(d){
 								observable(d.format(options.format));
 							}
-						});
-					},
-					update: function (element, valueAccessor) {
-						var value = ko.unwrap(valueAccessor());
-						$(element).datetimepicker('date', value || '').val(value || '');
-					}
+							else{
+								observable(null);
+							}
+						}
+					});
+				},
+				update: function (element, valueAccessor) {
+					var value = ko.unwrap(valueAccessor());
+					$(element).datetimepicker('date', value || '').val(value || '');
+				}
 			};
 
 		});
