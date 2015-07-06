5165455327ae16fa5ab4e9c566aceae5d7432e68
Peter Bunyan <pete@blueshed.co.uk>
2015-07-06 18:07:02 +0100

    build 15
    more menu fun

--- src/main.js
+++ src/main.js
@@ -44,6 +44,62 @@ define(["knockout",
 		Appl.prototype.notify = function(message, type, location, fade, duration){
 			return notify(message, type, location, fade, duration);
 		};
+    	
+    	Appl.prototype.add_service_menu = function(label, component, route, icon, href){
+    		return this.add_page(label,function(){
+    			this.component_params.args=arguments;
+        		this.component(component);
+        	}.bind(this),route,href,icon);
+    	};
+
+		Appl.prototype.add_menu_left = function(label, component, route, icon, href){
+			return this.routes.add_to_left_menu({
+				route:route,  // the name to pass to hasher
+				title:label,  // the menu title
+				href:href,    // the href for the menu item
+				icon:icon,    // the fa icon to set in menu item
+				action: function(){
+					this.component_params.args=arguments;
+		        	this.component(component);
+		        }.bind(this)
+			});
+		};
+
+		Appl.prototype.add_menu_right = function(label, component, route, icon, href){
+			return this.routes.add_to_right_menu({
+				route:route,  // the name to pass to hasher
+				title:label,  // the menu title
+				href:href,    // the href for the menu item
+				icon:icon,    // the fa icon to set in menu item
+				action: function(){
+					this.component_params.args=arguments;
+		        	this.component(component);
+		        }.bind(this)
+			});
+		};
+
+		Appl.prototype.extend_path = function(values){
+			var menu = this.routes.current_menu();
+			if(menu && menu.route){
+				var path = menu.href;
+				if(values && values.length > 0){
+					var extension = values.join("/");
+					path = path + "/" + extension;
+				}
+				if(path.indexOf("#/") == 0){
+					path = path.substring(2);
+				}
+				this.routes.set_hash_silently(path);
+			}
+		};
+		
+		Appl.prototype.content_height = function(delta){
+			var height = $(window).height();
+			if(delta){
+				height = height + delta;
+			}
+			return height + "px";
+		};
 
 	return Appl;
 });
