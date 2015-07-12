d95276922e14ad55e8d75c910cf3969a3a65620c
Peter Bunyan <pete@blueshed.co.uk>
2015-07-12 13:38:19 +0100

    build 19 - with modeling and inspector

---
+++ src/components/inspector/cursor.js
@@ -0,0 +1,217 @@
+define([
+		"knockout",
+		"text!./main-tmpl.html",
+		"blueshed/notify"
+	],
+	function(ko, main_tmpl, notify){
+
+		var MANY_DIRECTIONS = ["ONETOMANY","MANYTOMANY"];
+
+		function Cursor(params){
+			this.store = params.store;
+			this.model = this.store.model;
+			this.selected = ko.observable();
+			this.query_entity = ko.observable();
+			this.selected_data = ko.observable();
+
+
+			this.order_by = ko.observable();
+			this.order_by_asc = ko.observable(false);
+			this.offset = ko.observable(0);
+			this.limit = ko.observable(10);
+			this.total = ko.observable(0);
+			this.filter_total = ko.observable(0);
+
+			this.id = ko.observable();
+			this.attribute = ko.observable();
+			this.attribute_value = ko.observable();
+
+			this.items = ko.observableArray();
+			this.visible_columns = ko.observableArray();
+			this.order_columns = ko.observableArray();
+
+			this.all_columns = ko.computed(function(){
+				var result = [];
+				if(this.selected()){
+					result = this.selected().scalars.
+						filter(function(item){
+							return item.fkey!==true;
+						},this).
+						map(function(item){
+						var col = {
+							search: ko.observable()
+						};
+						for(name in item){
+							col[name] = item[name];
+						}
+						return col;
+					});
+				}
+				this.visible_columns(result.slice(0));
+				this.order_columns(result.slice(0));
+				return result;
+			},this);
+			this.columns = ko.pureComputed(function(){
+				return this.order_columns().filter(function(item){
+					return this.visible_columns.indexOf(item) != -1;
+				},this).slice(0);
+			},this);
+			this.many_relations = ko.pureComputed(function(){
+				if(this.selected()){
+					return this.selected().relations.filter(function(item){
+						return MANY_DIRECTIONS.indexOf(item.direction) != -1;
+					},this);
+				}
+			},this);
+
+
+			this.sub = this.selected.subscribe(function(item){
+				this.reset();
+				this.load();
+			},this);
+		}
+
+		Cursor.prototype.dispose = function() {
+			this.sub.dispose();
+		};
+
+		Cursor.prototype.load_entity = function(entity, match, value) {
+			this.reset();
+			this.attribute(match || null);
+			this.attribute_value(value || null);
+			this.id(null);
+			if(this.selected() != entity){
+				this.query_entity(entity);	
+				this.selected(entity);	
+			} else {
+				this.load();
+			}
+		};
+
+		Cursor.prototype.load_entity_attribute = function(entity, attribute, value) {
+			this.reset();
+			this.attribute(attribute.name);
+			this.id(value);
+			var result_entity = this.find(attribute.type);
+			if(this.query_entity() != entity){
+				this.query_entity(entity);	
+				this.selected(result_entity);	
+			} else {
+				if(this.selected() != result_entity){
+					this.selected(result_entity);	
+				}
+				this.load();
+			}
+		};
+
+		Cursor.prototype.find = function(name) {
+			return this.model().find(function(item){
+				return item.name == name;
+			},this);
+		};
+
+		Cursor.prototype.find_property = function(name) {
+			return this.properties().find(function(item){
+				return item.name == name;
+			},this);
+		};
+
+		Cursor.prototype.reset = function() {
+			this.order_by(null);
+			this.order_by_asc(false);
+			this.offset(0);
+			this.limit(10);
+			this.total(0);
+			this.filter_total(0);
+			this.selected_data(null);
+			this.items.removeAll();
+		};
+
+		Cursor.prototype.load = function() {
+			if(!this.selected()){
+				return;
+			}
+			var type = this.query_entity().name;
+            this.store.fetch({
+        		type: type,
+        		id: this.id(),
+        		attr: this.attribute(),
+        		match: this.attribute_value(),
+        		offset: this.offset(), 
+        		limit: this.limit(),
+        		order_by: this.order_by(),
+        		order_by_asc: this.order_by_asc(),
+        		depth:1
+            },
+            function(result){
+            	this.selected_data(null);
+            	this.items.removeAll();
+                this.items(result.rows);
+                if(this.attribute() && this.attribute_value()){
+                	this.filter_total(result.filter_total);
+                	this.total(result.filter_total);
+					notify("found " + result.filter_total + " " + type);
+                } else {
+                	this.filter_total(result.filter_total || result.total);
+                	this.total(result.total);
+                	notify("found " + result.total + " " + type);
+                }
+            }.bind(this));
+		};
+
+		Cursor.prototype.next = function(){
+			var new_offset = this.offset() + this.limit();
+			if(new_offset > this.total()){
+				new_offset = 0;
+			}
+			this.offset(new_offset);
+			this.load();
+		};
+
+		Cursor.prototype.prev = function(){
+			var new_offset = this.offset() - this.limit();
+			if(new_offset < 0){
+				new_offset = 0;
+			}
+			this.offset(new_offset);
+			this.load();
+		};
+
+		Cursor.prototype.change_order_by = function(value) {
+			if(this.selected()){
+				if(this.order_by() == value){
+					this.order_by_asc(!this.order_by_asc());
+				} else {
+					this.order_by_asc(false);
+					this.order_by(value);
+				}
+				this.load();
+			}
+		};
+
+		Cursor.prototype.surrogate_for = function(item){
+			if(item){
+				var text = item._type + "[" + ko.unwrap(item.id) + "]";
+				if(item.name){
+					text =ko.unwrap(item.name);
+				} else if(item.label){
+					text =ko.unwrap(item.label);
+				}
+				return {
+					_type: item._type,
+					id: ko.unwrap(item.id),
+					text: text
+				};
+			}
+		};
+
+		Cursor.prototype.surrogate_text = function(item){
+			var result = this.surrogate_for(item);
+			if(result){
+				return result.text;
+			}
+		};
+
+		return Cursor;
+	}
+);
---
+++ src/components/inspector/main.js
@@ -0,0 +1,181 @@
+define([
+		"knockout",
+		"text!./main-tmpl.html",
+		"./cursor",
+		"blueshed/editor",
+		"bridge/model/colours",
+		"text!./cursor-table-tmpl.html",
+		"text!./editor-form-tmpl.html",
+		"text!./field-selector-tmpl.html",
+		"blueshed/bindings/ko-popover",
+		"knockout-sortable",
+		"knockout-switch-case"
+	],
+	function(ko, main_tmpl, Cursor, Editor, colours,
+			cursor_table_tmpl,
+			editor_form_tmpl,
+			field_selector_tmpl){
+
+
+		ko.components.register("cursor-table",{template:cursor_table_tmpl});
+		ko.components.register("editor-form",{template:editor_form_tmpl});
+		$('head').append('<script type="text/html" id="inspector-field-selector">'+
+				field_selector_tmpl+'</script>');
+
+
+		function Panel(params){
+			window.panel = this;
+			this.appl = params.appl;
+			this.cursor = new Cursor({store:this.appl.store});
+			this.show_types = ko.observable(0);
+			this.display_many = ko.observable();
+			this.many_cursor = new Cursor({store:this.appl.store});
+
+			this.editing = ko.observable();
+			this.colours = colours;
+
+			this.update_many_model = ko.computed(function(){
+				if(this.display_many() && 
+				   this.cursor.selected() && 
+				   this.cursor.selected_data()){
+				   	if(this.display_many().direction == "ONETOMANY"){
+						this.many_cursor.load_entity(
+							this.cursor.find(this.display_many().type),
+							this.display_many().fkey,
+							this.cursor.selected_data().id);
+					} else {
+						this.many_cursor.load_entity_attribute(
+							this.cursor.selected(),
+							this.display_many(),
+							this.cursor.selected_data().id);
+					}
+					return this.many_cursor;
+				}
+			},this);
+
+			// respond to navigation
+			if(params.args.length){
+				var name = params.args[0];
+				var item = this.cursor.find(name);
+				this.change_entity(item);
+			}
+
+			this.subs = [
+				this.cursor.selected.subscribe(function(item){
+					// adjust naviagtion
+	            	if(item){
+						this.appl.extend_path([item.name]);
+					} else {
+						this.appl.extend_path();
+					}
+				},this),
+				this.cursor.selected_data.subscribe(function(item){
+					if(!item){
+						this.many_cursor.reset();
+					}
+				},this)
+			];
+		}
+
+		Panel.prototype.init = function() {
+			
+		};
+
+		Panel.prototype.dispose = function() {
+			this.subs.map(function(item){
+				item.dispose();	
+			});
+			this.many_cursor.dispose();
+			this.cursor.dispose();
+		};
+
+		Panel.prototype.change_entity = function(value) {
+			this.many_cursor.reset();
+			this.display_many(null);
+			this.cursor.load_entity(value);
+		};
+
+		Panel.prototype.add = function() {
+			var seed = this.cursor.store.seed_for(this.cursor.selected().name);
+			var meta = this.cursor.selected();
+			this.editing(new Editor(seed,meta));
+		};
+
+		Panel.prototype.edit = function() {
+			var item = this.cursor.selected_data();
+			this.editing(item ? new Editor(ko.toJS(item), 
+										   this.cursor.selected()) : null);
+		};
+
+		Panel.prototype.save = function() {
+			this.cursor.store.save(this.editing().save(), function(result){
+				this.editing(null);
+			}.bind(this));
+		};
+
+		Panel.prototype.cancel = function() {
+			this.editing(null);
+		};
+
+		Panel.prototype.remove = function() {
+			if(!this.editing().id){
+				this.close();
+			} else {
+				this.appl.confirm("Remove Project","Are you sure you want to remove this project?",function(){
+					this.appl.close_dialog();
+					this.appl.store.remove(this.selected(), function(){
+						this.close();
+						this.appl.notify("Removed!");
+					}.bind(this));
+				}.bind(this),"Remove","danger");
+			}
+		};
+
+		Panel.prototype.filter_for = function(meta, query, callback, err_back){
+			var target_meta = this.cursor.find(meta.type);
+			var field = target_meta.fields.find(function(item){
+				return item.name=="name" || item.name=="label";
+			});
+			var args = { type: meta.type };
+			if(field){
+				args.filter = query.data.q;
+				args.attr = field.name;
+			} else {
+				args.match = query.data.q;
+				args.attr = "id";
+			}
+			this.appl.store._fetch(args,
+				function(result){
+					callback({
+						results: result.rows.map(function(item){
+							return this.cursor.surrogate_for(item);
+						}.bind(this))
+					});
+				}.bind(this));
+
+		};
+
+		Panel.prototype.placeholder_for = function(meta, data){
+			return this.cursor.surrogate_for(data);
+		};
+
+		Panel.prototype.photo_args = function(meta){
+			return this.appl.store.photo_args(this.editing().photo,meta.type);
+		};
+
+        Panel.prototype.open_photo = function(title,src){
+        	src = ko.unwrap(src);
+        	if(src){
+        		this.appl.open_dialog("photo-dialog",{
+        			src: src.replace("/small.","/original."),
+        			title: title
+        		});
+        	}
+        };
+
+		return {
+			template: main_tmpl,
+			viewModel: Panel
+		};
+	}
+);
